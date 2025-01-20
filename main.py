6bb32df51dc41e1b000bd5e53f7d68899a0050a4
vinaysurtani <vinaysurtani@gmail.com>
2025-01-20 09:10:49 +0530

    added more player functionality

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import os
 os.environ["SDL_AUDIODRIVER"] = "dummy"
+from player import Player
 import pygame
 from constants import *
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+		for update in updatable:
+			update.update(dt)
+		for draw in drawable:
+			draw.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
 	main()
